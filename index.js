ae57aaca834c0d57aff47d9e65a450198c2e55a2
Eduart2021 <zeedymarina@yahoo.com>
2022-04-02 18:20:16 -0400

    port

--- index.js
+++ index.js
@@ -131,5 +131,5 @@ app.get('/news/:newspaperid', (req, res) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=> console.log(`server started on port ${PORT}`))
